7ce0e61a993e50c54a17b7d2f45cccef64539621
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-06-24 02:15:18 +0700

    Avoid zero division

--- vabsa/models/metrics.py
+++ vabsa/models/metrics.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def multioutput_f1(y_pred, y):
+def f1(y_pred, y):
     final_f1 = 0
+    eps = 1e-7
     for i in range(len(y)):
-        precision = np.sum(np.where(y_pred[i] == y[i], y[i] != 0, False)) / np.sum(np.where(y_pred[i] != 0, 1, 0))
-        recall = np.sum(np.where(y_pred[i] == y[i], y[i] != 0, False)) / np.sum(np.where(y[i] != 0, 1, 0))
-        f1 = 2 * precision * recall / (precision + recall)
+        precision = np.sum(np.where(y_pred[i] == y[i], y[i] != 0, False)) / (np.sum(np.where(y_pred[i] != 0, 1, 0)) + eps)
+        recall = np.sum(np.where(y_pred[i] == y[i], y[i] != 0, False)) / (np.sum(np.where(y[i] != 0, 1, 0)) + eps)
+        f1 = 2 * precision * recall / (precision + recall + eps)
         final_f1 += f1
     return final_f1 / len(y_pred)
         
